70f2240fae5ed0a6a8d80b59a8f0950afe2442c9
Binal Patel <binalkp91@gmail.com>
2022-12-22 21:48:26 +0000

    reverted some testing code

--- 2_extracting_and_structuring_recipes/recipe_parser.py
+++ 2_extracting_and_structuring_recipes/recipe_parser.py
@@ -1,10 +1,10 @@
 import json
 
+from langchain.chains import LLMChain
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain
 
-llm = OpenAI(temperature=0, max_tokens=1000)  # noqa
+llm = OpenAI(temperature=0, max_tokens=1000)  # type: ignore
 
 
 def parse_recipe(recipe_text: str) -> dict:
@@ -14,7 +14,7 @@ def parse_recipe(recipe_text: str) -> dict:
         input_variables=["recipe"],
         template="""{recipe}
         
-        Extract the ingredients and instructions from the above, output as json with keys lowercase keys instructions and ingredients. For ingredients extract quantity if available. """
+        Extract the ingredients and instructions from the above, output as json with keys lowercase keys instructions and ingredients. For ingredients extract quantity if available. """,  # noqa
     )
 
     # secondary prompt to group extracted ingredients into categories
@@ -23,32 +23,18 @@ def parse_recipe(recipe_text: str) -> dict:
         template="""
         {ingredients}
         
-        Group above ingredients into similar categories, output as json with lowercase keys categories:"""
-    )
-
-    nutrition_prompt = PromptTemplate(
-        input_variables=["recipe"],
-        template="""
-        {recipe}
-
-        Generate the nutrition info for the above receipt and whether or not it's healthy.
-        """
+        Group above ingredients into similar categories, output as json with lowercase keys categories:""",  # noqa
     )
 
     extract_chain = LLMChain(prompt=extract_prompt, llm=llm)
     grouping_chain = LLMChain(prompt=grouping_prompt, llm=llm)
-    nutrition_chain = LLMChain(prompt=nutrition_prompt, llm=llm)
 
     text_result = extract_chain.run(recipe_text)
     json_result = json.loads(text_result)
-    print(json_result)
 
-    ingredient_group_text = grouping_chain.run(
-        json.dumps(json_result['ingredients']))
+    ingredient_group_text = grouping_chain.run(json.dumps(json_result["ingredients"]))
     ingredient_group_json = json.loads(ingredient_group_text)
 
-    json_result['grouped_ingredients'] = ingredient_group_json
+    json_result["grouped_ingredients"] = ingredient_group_json
 
-    nutrition_info = nutrition_chain(json.dumps(json_result))
-
-    return nutrition_info
+    return json_result
